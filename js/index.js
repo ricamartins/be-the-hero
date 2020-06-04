fd14a8061827661ed7d91db042edefd2fdfa0cbb
Rebeca Moreira <38115101+adiosre@users.noreply.github.com>
2020-06-04 17:15:47 -0300

    adicionando select de estado e cidade

--- js/index.js
+++ js/index.js
@@ -63,6 +63,8 @@ let form = selectId('formulario');
 let nome  = selectId('nome');
 let email  = selectId('email');
 let mensagem  = selectId('mensagem');
+let uf = selectId('uf');
+let city = selectId('city');
 
 let errorListUl = document.querySelector('#error-list ul');
 let errorList = selectId('error-list');
@@ -79,8 +81,18 @@ form.addEventListener("submit", function (ev){
         errorMessage("Campo <b>email</b> não preenchido");
     }
 
+
     if (empty(mensagem)){
         errorMessage("Campo <b>mensagem</b> não preenchido");
+    }
+
+    
+    if (empty(uf)){
+        errorMessage("Campo <b>uf</b> não selecionado");
+    }
+
+    if (empty(city)){
+        errorMessage("Campo <b>cidade</b> não selecionado");
     }
 
     if (errorListUl.querySelectorAll('li').length > 0){
@@ -90,3 +102,52 @@ form.addEventListener("submit", function (ev){
 
 
 });
+
+
+/*form estado/cidade */
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res =>  res.json() )
+    .then( states => {
+
+        for ( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+
+populateUFs()
+
+
+function getCities (event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedValue = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedValue].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then( res =>  res.json() )
+    .then( cities => {
+
+        for ( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    })
+}
+
+
+
+/*quando ocorrer uma mudança ele executará a função getcities */
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
